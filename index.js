b99481f3c86bfc0e89548571ccf0d1cb63a78ff4
AhmadHirthani <hertani86@gmail.com>
2020-10-04 22:50:36 +0300

    finishAtlas

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-  
-'use strict';
-require('dotenv').config();
-const mongoose = require('mongoose');
+  'use strict';
+  require('dotenv').config();
+  const mongoose = require('mongoose');
 
-const serverModule = require('./server'); 
+  const serverModule = require('./server');
 
-const MONGOOSE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/hopes'; 
+  // const MONGOOSE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/hopes'; 
 
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-};
-mongoose.connect(MONGOOSE_URL, mongooseOptions);
+  // const mongooseOptions = {
+  //   useNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
+  //   useUnifiedTopology: true,
+  // };
+  // mongoose.connect(MONGOOSE_URL, mongooseOptions);
 
 
-serverModule.start();
+  //connect to cloud DB
 
+
+
+  const uri = "mongodb+srv://ahmad:hopes2020@cluster0.vagkk.mongodb.net/hopes?retryWrites=true&w=majority";
+
+
+  // const uri = "mongodb + srv: //ahmad:hopes2020@cluster0.vagkk.mongodb.net/hopes?retryWrites=true&w=majority";
+  mongoose.connect(uri, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      })
+      .then(() => {
+          console.log('MongoDB Connected…')
+      })
+      .catch(err => console.log(err))
+
+  // const MongoClient = require('mongodb').MongoClient;
+  // const uri = "mongodb+srv://ahmad:hopes2020@cluster0.vagkk.mongodb.net/hopes?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  // client.connect(err => {
+  //     const collection = client.db("test").collection("devices");
+  //     // perform actions on the collection object
+  //     client.close();
+  // });
+
+
+
+
+
+
+
+
+
+
+  serverModule.start();
