cbeb91f6c9fe4c454a271fd877ef9c5681d8b7fc
roukiaSalahi <roqaia.sa@hotmail.com>
2020-10-06 16:10:24 +0300

    fix add comments

--- index.js
+++ index.js
@@ -4,18 +4,18 @@
 
   const serverModule = require('./server');
 
-//   const MONGOOSE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/hopes'; 
+  const MONGOOSE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/hopes'; 
 
-//   const mongooseOptions = {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   };
-//   mongoose.connect(MONGOOSE_URL, mongooseOptions);
+  // const mongooseOptions = {
+  //   useNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   useFindAndModify: false,
+  //   useUnifiedTopology: true,
+  // };
+  // mongoose.connect(MONGOOSE_URL, mongooseOptions);
 
 
-  //   connect to cloud DB
+    // connect to cloud DB
    const uri = "mongodb+srv://ahmad:hopes2020@cluster0.vagkk.mongodb.net/hopes?retryWrites=true&w=majority";
    mongoose.connect(uri, {
            useNewUrlParser: true,
