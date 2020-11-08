c3594e938e3667ad2c02fd270f299d3fe3c65fdf
AhmadHirthani <hertani86@gmail.com>
2020-11-08 22:11:43 +0200

    commentRouterInServerJs

--- server.js
+++ server.js
@@ -24,114 +24,114 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index'));
 // routes to handle payments
-app.post('/pay', handlePayment);
-app.get('/success', handleSuccess);
-app.get('/cancel', (req, res) => res.send('Cancelled'));
+// app.post('/pay', handlePayment);
+// app.get('/success', handleSuccess);
+// app.get('/cancel', (req, res) => res.send('Cancelled'));
 // paypal configuration 
-paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': process.env.PAYPAL_CLIENT_ID,
-    'client_secret': process.env.PAYPAL_CLIENT_SECRET,
-    'headers': {
-        'custom': 'header'
-    }
-});
+// paypal.configure({
+//     'mode': 'sandbox', //sandbox or live
+//     'client_id': process.env.PAYPAL_CLIENT_ID,
+//     'client_secret': process.env.PAYPAL_CLIENT_SECRET,
+//     'headers': {
+//         'custom': 'header'
+//     }
+// });
 // ======================================= handeling payments functions :
-function handlePayment(req, res, next) {
-    console.log("handlePayment called");
-    amount = req.body.amount;
-    inNeedEmail = req.body.email;
-    inNeeduserId = req.body.userId;
+// function handlePayment(req, res, next) {
+//     console.log("handlePayment called");
+//     amount = req.body.amount;
+//     inNeedEmail = req.body.email;
+//     inNeeduserId = req.body.userId;
+//     userId
 
+//     console.log({ amount });
+//     console.log({ inNeedEmail });
+//     console.log({ inNeeduserId });
 
-    console.log({ amount });
-    console.log({ inNeedEmail });
-    console.log({ inNeeduserId });
-
-    const create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
-        },
-        "redirect_urls": {
-            "return_url": process.env.SUCCESS_URL,
-            "cancel_url": process.env.CANCEL_URL,
-        },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "Red Sox Hat",
-                    "sku": "001",
-                    "price": amount,
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": amount
-            },
-            "description": "Hat for the best team ever"
-        }]
-    };
-    paypal.payment.create(create_payment_json, function(error, payment) {
-        console.log('payment create called');
-        try {
-            for (let i = 0; i < payment.links.length; i++) {
-                if (payment.links[i].rel === 'approval_url') {
-                    res.redirect(payment.links[i].href);
-                }
-            }
-        } catch (error) {
-            console.log("create payment error>>>", error);
-        }
-        // res.send("test");
-    });
-}
-function handleSuccess(req, res, next) {
-    console.log('success called');
-    const payerId = req.query.PayerID;
-    const paymentId = req.query.paymentId;
-    const execute_payment_json = {
-        "payer_id": payerId,
-        "transactions": [{
-            "amount": {
-                "currency": "USD",
-                "total": amount
-            }
-        }]
-    };
-    paypal.payment.execute(paymentId, execute_payment_json, function(error, payment) {
-        console.log('execute called');
-        if (error) {
-            console.log(error.response);
-            throw error;
-        } else {
-            var mailOptions = {
-                from: 'hopegaza12@gmail.com',
-                to: inNeedEmail,
-                subject: 'you hava a donation',
-                text: `you have received a donate
-                 from ${payment.payer.payer_info.first_name} ${payment.payer.payer_info.last_name}, 
-                 with the amount of ${payment.transactions[0].amount.total} ${payment.transactions[0].amount.currency}`
-            };
-            sendMail(mailOptions);
-            console.log(JSON.stringify(payment));
-            let obj = {
-                userId: payment.transactions[0].payee.merchant_id,
-                date: payment.create_time,
-                donorName: payment.payer.payer_info.first_name + ' ' + payment.payer.payer_info.last_name,
-                amount: payment.transactions[0].amount.total,
-                currency: payment.transactions[0].amount.currency
-            }
-            payments.create(obj).then(result => {
-                console.log(result);
-                res.send('Success');
-            });
-            // res.send('Success');
-        }
-    });
-}
+//     const create_payment_json = {
+//         "intent": "sale",
+//         "payer": {
+//             "payment_method": "paypal"
+//         },
+//         "redirect_urls": {
+//             "return_url": process.env.SUCCESS_URL,
+//             "cancel_url": process.env.CANCEL_URL,
+//         },
+//         "transactions": [{
+//             "item_list": {
+//                 "items": [{
+//                     "name": "Red Sox Hat",
+//                     "sku": "001",
+//                     "price": amount,
+//                     "currency": "USD",
+//                     "quantity": 1
+//                 }]
+//             },
+//             "amount": {
+//                 "currency": "USD",
+//                 "total": amount
+//             },
+//             "description": "Hat for the best team ever"
+//         }]
+//     };
+//     paypal.payment.create(create_payment_json, function(error, payment) {
+//         console.log('payment create called');
+//         try {
+//             for (let i = 0; i < payment.links.length; i++) {
+//                 if (payment.links[i].rel === 'approval_url') {
+//                     res.redirect(payment.links[i].href);
+//                 }
+//             }
+//         } catch (error) {
+//             console.log("create payment error>>>", error);
+//         }
+//         // res.send("test");
+//     });
+// }
+// function handleSuccess(req, res, next) {
+//     console.log('success called');
+//     const payerId = req.query.PayerID;
+//     const paymentId = req.query.paymentId;
+//     const execute_payment_json = {
+//         "payer_id": payerId,
+//         "transactions": [{
+//             "amount": {
+//                 "currency": "USD",
+//                 "total": amount
+//             }
+//         }]
+//     };
+//     paypal.payment.execute(paymentId, execute_payment_json, function(error, payment) {
+//         console.log('execute called');
+//         if (error) {
+//             console.log(error.response);
+//             throw error;
+//         } else {
+//             var mailOptions = {
+//                 from: 'hopegaza12@gmail.com',
+//                 to: inNeedEmail,
+//                 subject: 'you hava a donation',
+//                 text: `you have received a donate
+//                  from ${payment.payer.payer_info.first_name} ${payment.payer.payer_info.last_name}, 
+//                  with the amount of ${payment.transactions[0].amount.total} ${payment.transactions[0].amount.currency}`
+//             };
+//             sendMail(mailOptions);
+//             console.log(JSON.stringify(payment));
+//             let obj = {
+//                 userId: payment.transactions[0].payee.merchant_id,
+//                 date: payment.create_time,
+//                 donorName: payment.payer.payer_info.first_name + ' ' + payment.payer.payer_info.last_name,
+//                 amount: payment.transactions[0].amount.total,
+//                 currency: payment.transactions[0].amount.currency
+//             }
+//             payments.create(obj).then(result => {
+//                 console.log(result);
+//                 res.send('Success');
+//             });
+//             // res.send('Success');
+//         }
+//     });
+// }
 //-----------------------------------------------------------
 app.use(cors());
 app.use(morgan('dev'));
